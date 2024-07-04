11d48441c377fcbe928ec3cb0514e495205e9d88
snowb-x <77083798+snowb-x@users.noreply.github.com>
2024-07-04 11:06:26 +1000

    done something

--- main/src/objs/background_stars.c
+++ main/src/objs/background_stars.c
@@ -1,3 +1,4 @@
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,7 @@
 #include "raymath.h"
 
 #include "../base.h"
+#include "../settings.h"
 
 #ifndef _camera
 #define _camera
@@ -22,90 +24,42 @@
 #include "src/objs/particle.c"
 #endif
 
-#ifndef _sprite
-#define _sprite
-#include "src/sprite.c"
+#ifndef _misc
+#define _misc
+#include "../misc.c"
 #endif
 
 typedef struct
 {
-    float a;
-    float tmr;
+    long randomSeed;
+    int minStars;
+    int maxStars;
+} BackgroundStars_Data;
 
-    Sprite *sprite;
-} ExampleObject_Data;
+#define BACKGROUND_STARS_DATA ((BackgroundStars_Data *)(THIS->data_struct))
 
 int _BackgroundStars_Init(void *self, float DeltaTime)
 {
-    // we have a reference to our own gameobject from which we can do things.
-    // here we should create a reference to our datastructure and store it in the data_struct pointer.
-
-    ExampleObject_Data *data = malloc(sizeof(ExampleObject_Data));
-
-    data->a = 0;
-    data->tmr = 0;
-    data->sprite = CreateSprite("resources/kitr_temp.png");
-
-    ((GameObj_Base *)self)->data_struct = (void *)data;
+    // declar data
 
     return 0;
 }
 
 int _BackgroundStars_Update(void *self, float DeltaTime)
 {
-    // we can cast our data struct to the right data like so:
-    ExampleObject_Data *data = THIS->data_struct;
-
-    THIS->position.x = (float)(sin(data->a * (PI / 2)) * 5);
-    data->a += DeltaTime;
-
-    // An example of spawning a particle
-    data->tmr += DeltaTime;
-    if (data->tmr >= 0.4)
-    {
-        data->tmr = 0;
-        SpawnParticle(((GameObj_Base *)self)->position, (Vector2){0, 0}, (Vector2){0, 1},
-                      (Vector2){0.125, 0.125}, 2,
-                      (Color){255, 127, 0, 127}, 1);
-    }
-
-    // An example of searching for objects with neutral flag.
-    for (int i = 0; i != -1;)
-    {
-        GameObj_Base *obj;
-        i = GetObjectWithFlagsAny(FLAG_NEUTRAL_OBJECT, i, &obj);
-
-        // Check if obj is not null
-        if (!obj || i == -1)
-            break;
-
-        // Do an operation with the result...
-    }
 
     return 0;
 }
 
 int _BackgroundStars_Draw(void *self, float DeltaTime)
 {
-    // ibid
-    GameObj_Base *selfObj = ((GameObj_Base *)self);
-    ExampleObject_Data *data = ((GameObj_Base *)self)->data_struct;
-
-    RenderSpriteRelative(data->sprite, selfObj->position, selfObj->size, sin(data->a) * 90, WHITE);
-    // RenderSquareRelative(selfObj->position, selfObj->size, 0, WHITE);
-
+    // draw the random stars
     return 0;
 }
 
 int _BackgroundStars_Destroy(void *self, float DeltaTime)
 {
-    // ibid.
-    // if you malloc anything, destroy it here. this includes your data package.
-
-    // free our data struct here. free anything contained.
-    ExampleObject_Data *data = ((GameObj_Base *)self)->data_struct;
-    free(data->sprite);
-    free(data);
+    free(BACKGROUND_STARS_DATA);
 
     return 0;
 }
