f0bc3e55826859c95608dae20b04bc702f3ee976
hmc-rae <hcough.mcrae@gmail.com>
2024-07-06 19:21:07 +1000

    Made layer enum work cooler

--- main/src/obj_register.h
+++ main/src/obj_register.h
@@ -15,19 +15,19 @@ enum OBJECT_FLAG {
 };
 
 enum LAYER_ID {
-    LAYER_BACKGROUND_STARSCAPE_0 = 0,
-    LAYER_BACKGROUND_STARSCAPE_1 = 1,
-    LAYER_BACKGROUND_STARSCAPE_2 = 2,
-    LAYER_BACKGROUND_STARSCAPE_3 = 2,
-    LAYER_BACKGROUND_STARSCAPE_4 = 2,
-    LAYER_BACKGROUND_STARSCAPE_5 = 2,
-    LAYER_BACKGROUND_OBJECTS = 3,
-    LAYER_WORMHOLE = 4,
-    LAYER_ASTEROIDS = 5,
-    LAYER_STARS = 6,
-    LAYER_PARTICLES = 7,
-    LAYER_PLAYER = 8,
-    LAYER_GUI = 9,
+    LAYER_BACKGROUND_STARSCAPE_0,
+    LAYER_BACKGROUND_STARSCAPE_1,
+    LAYER_BACKGROUND_STARSCAPE_2,
+    LAYER_BACKGROUND_STARSCAPE_3,
+    LAYER_BACKGROUND_STARSCAPE_4,
+    LAYER_BACKGROUND_STARSCAPE_5,
+    LAYER_BACKGROUND_OBJECTS,
+    LAYER_WORMHOLE,
+    LAYER_ASTEROIDS,
+    LAYER_STARS,
+    LAYER_PARTICLES,
+    LAYER_PLAYER,
+    LAYER_GUI,
 
     // DO NOT, I REPEAT, DO NOT REMOVE OR OVERWRITE LAYER_BOOKEND!
     // THIS SHOULD ALWAYS BE AT THE END OF THE ENUM!!! DO NOT REMOVE OR OVERWRITE ME!!! 
@@ -59,3 +59,18 @@ enum PLAYER_LIFE_STATUS {
     PLAYER_LIFE_STATUS_ISHULL = 1,
     PLAYER_LIFE_STATUS_ISSHIELDED = 2
 };
+
+
+
+// ~~~~~~~~~~ CREATE FUNCTIONS FOR EACH OBJ ~~~~~~~~~~
+// Add all creates for each objs file here.
+
+GameObj_Base* CreateAsteroid();
+GameObj_Base* CreateBackgroundStars(enum LAYER_ID layer, float objectScale);
+GameObj_Base* CreateBackgroundSprites();
+GameObj_Base* CreateExampleObject();
+GameObj_Base* CreatePlayer();
+GameObj_Base* CreateParticleObject();
+GameObj_Base* CreatePlanet();
+GameObj_Base* CreateStarObject();
+GameObj_Base* CreateWormhole();
