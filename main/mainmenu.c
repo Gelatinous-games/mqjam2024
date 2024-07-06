418a14e7d686356fedeb456bdae45903475b0d60
Corbeau <aurora.griffith@students.mq.edu.au>
2024-07-06 20:13:56 +1000

    aergbiurheag9ijre

--- main/mainmenu.c
+++ main/mainmenu.c
@@ -10,25 +10,45 @@
 #include "src/base.h"
 #include "src/obj_register.h"
 
+#define BUTTON_FONT_SIZE 20
+#define BUTTONLABELPADDING 10
+
+
+// uses top left for text
+
 Vector2 startButtonPos;
 Vector2 startButtonSize;
 Rectangle startButtonBounds;
+
+Vector2 startButtonTextPos;
+Vector2 startButtonTextSize;
+Rectangle startButtonTextBounds;
 const char *startButtonLabel = "START";
 
-
 int _MainMenu_Init(){
+    // button label
+    startButtonTextSize = (Vector2){
+       MeasureText(startButtonLabel, BUTTON_FONT_SIZE), BUTTON_FONT_SIZE
+    };
+    startButtonTextPos = (Vector2){
+        WINDOW_WIDTH/2, WINDOW_HEIGHT/2
+    };
+    startButtonTextBounds = (Rectangle){
+        startButtonTextPos.x - startButtonTextSize.x/2, startButtonTextPos.y - startButtonTextSize.y/2,
+        startButtonTextSize.x, startButtonTextSize.y
+    };
     // button
     startButtonPos = (Vector2){
         WINDOW_WIDTH/2, WINDOW_HEIGHT/2
     };
     startButtonSize = (Vector2){
-       200, 50
+       startButtonTextSize.x+(2*BUTTONLABELPADDING), startButtonTextSize.y+(2*BUTTONLABELPADDING)
     };
-
     startButtonBounds = (Rectangle){
         startButtonPos.x - startButtonSize.x/2, startButtonPos.y - startButtonSize.y/2,
         startButtonSize.x, startButtonSize.y
     };
+
 
 
 
@@ -47,7 +67,8 @@ int _MainMenu_Draw(){
     // DrawRectangleRec(startButtonBounds, WHITE);
     DrawRectangleRoundedLines(startButtonBounds, 0.0f, 4, 3.0f, DARKGRAY);
 
-    DrawText(startButtonLabel, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 20, WHITE);
+    DrawText(startButtonLabel, startButtonTextPos.x, startButtonTextPos.y, BUTTONLABELPADDING, WHITE);
+
 
     return 0;
 }
