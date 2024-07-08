9e169dacb31860b8bdf7ee695d6ad21e9902db8a
Corbeau <aurora.griffith@students.mq.edu.au>
2024-07-08 15:04:25 +1000

    half the texture library

--- main/src/texturemanager.c
+++ main/src/texturemanager.c
@@ -1,103 +1,82 @@
-#include "raylib.h"
+#include "texturemanager.h"
 
-enum _TEXTURELIBRARY_TextureType {
-    TEXTURETYPE_ASTEROID,
-    TEXTURETYPE_BGART,
-    TEXTURETYPE_BGBRUSH,
-    TEXTURETYPE_HEALTHBAR,
-    TEXTURETYPE_PLANET,
-    TEXTURETYPE_STAR,
-    TEXTURETYPE_WORMHOLE,
-    TEXTURETYPE_SHIP,
 
+_TEXTUREMANAGER_LIBRARYTREE_Node_ *_TEXTUREMANAGER_LIBRARYTREE_NewNode(char *pathOfTexture){
+    // ... allocates the node
+    _TEXTUREMANAGER_LIBRARYTREE_Node_ *newNode = (_TEXTUREMANAGER_LIBRARYTREE_Node_ *)malloc(sizeof(_TEXTUREMANAGER_LIBRARYTREE_Node_));
+    // and the data elem
+    newNode->data = (_TEXTUREMANAGER_LIBRARYTREE_NodeData_ *)malloc(sizeof(_TEXTUREMANAGER_LIBRARYTREE_NodeData_));
+    if(pathOfTexture){
+        // ... had path
+        newNode->data->texture = (Texture2D *)malloc(sizeof(Texture2D));
+        *(newNode->data->texture) = LoadTexture(pathOfTexture);
+        newNode->data->texturePathLength = sizeof(pathOfTexture);
+        newNode->data->texturePath = (char *)malloc(sizeof(newNode->data->texturePathLength));
+        strcpy((newNode->data->texturePath), pathOfTexture);
+    }
+    else {
+        // ... none path
+        newNode->data->texture = 0;
+        newNode->data->texturePath = 0;
+        newNode->data->texturePathLength = 0;
+    }
+    newNode->left = 0;
+    newNode->right = 0;
+}
+
+void _TEXTUREMANAGER_LIBRARYTREE_FreeNode(_TEXTUREMANAGER_LIBRARYTREE_Node_ **nodeToFree){
+    // real and exists
+    if(nodeToFree && *nodeToFree){
+        /// data exists
+        if((*nodeToFree)->data){
+            /// bin the path
+            free( (*nodeToFree)->data->texturePath );
+            (*nodeToFree)->data->texturePath = 0;
+            // bin texture
+            UnloadTexture( *((*nodeToFree)->data->texture) );
+            // bin texture pointer
+            free( (*nodeToFree)->data->texture );
+            (*nodeToFree)->data->texture = 0;
+            // bin node data pointer
+            free( (*nodeToFree)->data );
+            (*nodeToFree)->data = 0;
+        }
+
+        // this is assumed to already be done tho? dangerous if it isnt
+        // // detach the left and rights
+        // (*nodeToFree)->left = 0;
+        // (*nodeToFree)->right = 0;
+
+        // bin node pointer
+        free( (*nodeToFree) );
+        (*nodeToFree) = 0;
+    }
+    // done
+}
+
+int _TEXTUREMANAGER_LIBRARYTREE_CompareNode(_TEXTUREMANAGER_LIBRARYTREE_NodeData_ *self, _TEXTUREMANAGER_LIBRARYTREE_NodeData_ *other){
     // ...
-    TEXTURETYPE_COUNTER
-};
+    // TODO: aaa
+}
 
-
-#define _TEXTURELIBRARY_AsteroidTextureList_SIZE 4
-#define _TEXTURELIBRARY_BackgroundArtTextureList_SIZE 10
-#define _TEXTURELIBRARY_BackgroundBrushTextureList_SIZE 22
-#define _TEXTURELIBRARY_HealthBarTextureList_SIZE 22
-#define _TEXTURELIBRARY_PlanetTextureList_SIZE 5
-#define _TEXTURELIBRARY_StarTextureList_SIZE 5
-#define _TEXTURELIBRARY_WormholeTextureList_SIZE 1
-#define _TEXTURELIBRARY_ShipTextureList_SIZE 1
-
-Texture2D **_TEXTURELIBRARY_TextureLists;
-
-char **_TEXTURELIBRARY_TexturePathList;
-
-
-
-
-
-
-
-
-
-
-
-
-
-void InitialiseTextureLibrary(){
+_TEXTUREMANAGER_LIBRARYTREE_Node_ *_TEXTUREMANAGER_LIBRARYTREE_GetNodeFromPath(char *pathOfTexture){
     // ...
-    // Texture2D tex = LoadTexture(const char *);
+    // TODO: aaa 
+    // TODO: return the node pointer when found
+    // TODO: return null otherwise
+}
+
+int _TEXTUREMANAGER_LIBRARYTREE_PlaceNodeInTree(_TEXTUREMANAGER_LIBRARYTREE_Node_ *nodeToAdd){
+    // ...
+}
+
+void _TEXTUREMANAGER_Initialise(){
+    // ...
+    // TODO : make the root node
     
 }
 
-Texture2D *_TEXTURELIBRARY_GetTextureList(enum _TEXTURELIBRARY_TextureType typeIndex){
-    if(_TEXTURELIBRARY_TextureLists)
-    switch(typeIndex){
-        default:
-        case TEXTURETYPE_ASTEROID: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_ASTEROID];
-        case TEXTURETYPE_BGART: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_BGART];
-        case TEXTURETYPE_BGBRUSH: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_BGBRUSH];
-        case TEXTURETYPE_HEALTHBAR: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_HEALTHBAR];
-        case TEXTURETYPE_PLANET: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_PLANET];
-        case TEXTURETYPE_STAR: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_STAR];
-        case TEXTURETYPE_WORMHOLE: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_WORMHOLE];
-        case TEXTURETYPE_SHIP: return _TEXTURELIBRARY_TextureLists[TEXTURETYPE_SHIP];
-    }
-}
-
-// Texture2D *GetTexture(const char *TexturePath){
-//     for(int typeIndex = 0; typeIndex < TEXTURETYPE_COUNTER; typeIndex++){
-//         // ...
-//     }
-// }
-
-int _TEXTURELIBRARY_GetTextureListSize(enum _TEXTURELIBRARY_TextureType typeIndex){
-    if(_TEXTURELIBRARY_TextureLists)
-    switch(typeIndex){
-        default:
-        case TEXTURETYPE_ASTEROID: return _TEXTURELIBRARY_AsteroidTextureList_SIZE;
-        case TEXTURETYPE_BGART: return _TEXTURELIBRARY_BackgroundArtTextureList_SIZE;
-        case TEXTURETYPE_BGBRUSH: return _TEXTURELIBRARY_BackgroundBrushTextureList_SIZE;
-        case TEXTURETYPE_HEALTHBAR: return _TEXTURELIBRARY_HealthBarTextureList_SIZE;
-        case TEXTURETYPE_PLANET: return _TEXTURELIBRARY_PlanetTextureList_SIZE;
-        case TEXTURETYPE_STAR: return _TEXTURELIBRARY_StarTextureList_SIZE;
-        case TEXTURETYPE_WORMHOLE: return _TEXTURELIBRARY_WormholeTextureList_SIZE;
-        case TEXTURETYPE_SHIP: return _TEXTURELIBRARY_ShipTextureList_SIZE;
-    }
-}
-
-// TODO: have this do the grab from the list aaaa pain who does fast?
-// /// @brief 
-// /// @param TexturePath 
-// /// @return 
-// int GetTextureID(const char *TexturePath){
-//     // each list
-//     for(int i = 0; i < TEXTURETYPE_COUNTER; i++){
-//         // each item in the list
-//         for(int j = 0; j < _TEXTURELIBRARY_GetTextureListSize(i); j++){
-//             // TODO
-//         }
-//     }
-// }
-
-
-void DestroyTextureLibrary(){
+void _TEXTUREMANAGER_Destroy(){
     // ...
-    // UnloadTexture(tex);
+    // TODO : unallocate all nodes
 }
